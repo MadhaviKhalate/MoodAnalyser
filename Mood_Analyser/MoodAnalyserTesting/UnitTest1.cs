b74406b5abb5b1a0572191e6258a4bc791e0326b
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-06 20:02:10 +0530

    [Madhavi] test case to check mood

--- Mood_Analyser/MoodAnalyserTesting/UnitTest1.cs
+++ Mood_Analyser/MoodAnalyserTesting/UnitTest1.cs
@@ -10,5 +10,13 @@ namespace MoodAnalyserTesting
             string moodInput = "I am in Sad Mood";
             Assert.AreEqual("Sad", analyser.Mood(moodInput));
         }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Mood_Analyser.Analyser analyser = new Mood_Analyser.Analyser();
+            string moodInput = "I am in Any Mood";
+            Assert.AreEqual("Happy", analyser.Mood(moodInput));
+        }
     }
 }
