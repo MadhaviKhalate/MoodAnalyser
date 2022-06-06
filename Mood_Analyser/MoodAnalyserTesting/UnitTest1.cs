7cdce6c7cab202e058a124858b3ac27642f8a995
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-06 20:40:55 +0530

    [Madhavi] Refactor test case2

--- Mood_Analyser/MoodAnalyserTesting/UnitTest1.cs
+++ Mood_Analyser/MoodAnalyserTesting/UnitTest1.cs
@@ -4,11 +4,18 @@ namespace MoodAnalyserTesting
     {
 
         [Test]
-        public void CheckSad()
+        public void GetInputAsSad_AnalyseMood_ReturnSad()
         {
             string moodInput = "I am in Sad Mood";
             Mood_Analyser.Analyser analyser = new Mood_Analyser.Analyser(moodInput);
             Assert.AreEqual("Sad", analyser.Mood());
+        }
+        [Test]
+        public void GetInputAsHappy_AnalyseMood_ReturnHappy()
+        {
+            string moodInput = "I am in Any Mood";
+            Mood_Analyser.Analyser analyser = new Mood_Analyser.Analyser(moodInput);
+            Assert.AreEqual("Happy", analyser.Mood());
         }
     }
 }
