c1193ce47f0ac5c29e6d9282f2e459fdaa75e222
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-06 18:47:34 +0530

    [Madhavi] added code to analyse mood

---
+++ Mood_Analyser/Mood_Analyser/MoodAnalyser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyser
+{
+    public class MoodAnalyser
+    {
+        public string Mood(string input)
+        {
+            if (input.ToLower().Contains("sad"))
+            {
+                return "Sad";
+            }
+            return "Happy";
+        }
+    }
+}
---
+++ Mood_Analyser/Mood_Analyser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mood_Analyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Mood:");
+            string userInput = Console.ReadLine();
+            MoodAnalyser analyser = new MoodAnalyser();
+            Console.WriteLine("Your Mood: " + analyser.Mood(userInput));
+        }
+    }
+}
