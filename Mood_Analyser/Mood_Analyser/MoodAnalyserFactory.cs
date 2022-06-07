6487a9ff1bc034bbace6ea58e43c46ed7bbe35f0
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-07 20:06:34 +0530

    [Madhavi] code for mood analyser factory

---
+++ Mood_Analyser/Mood_Analyser/MoodAnalyserFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Mood_Analyser
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyse(string className,string constructorName)
+        {
+            string pattern = @"," + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new CustomException(CustomException.ExceptionTypes.NO_SUCH_CLASS, "Class Not Found");
+                }
+            }
+            else
+            {
+                throw new CustomException(CustomException.ExceptionTypes.NO_SUCH_CLASS, "Constructor is Not Found");
+            }
+        }
+    }
+}
