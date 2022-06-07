4d29aa142b27249a522d5ce82b1026626644b050
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-06-07 07:24:45 +0530

    [Madhavi] throw empty exception

--- Mood_Analyser/MoodAnalyserTesting/UnitTest1.cs
+++ Mood_Analyser/MoodAnalyserTesting/UnitTest1.cs
@@ -16,5 +16,19 @@ namespace MoodAnalyserTesting
                 Assert.AreEqual("Null Exception", ex.Message);
             }
         }
+        [Test]
+        public void GivenEmptyAsInput_AnalyseMood_ThrowEmptyException()
+        {
+            try
+            {
+                string input = "";
+                Mood_Analyser.Analyser analyser = new Mood_Analyser.Analyser(input);
+                string output = analyser.Mood();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Empty Exception", ex.Message);
+            }
+        }
     }
 }
